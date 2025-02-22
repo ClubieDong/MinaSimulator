fea75df357574de5bcbf1d980a1dbf3350f5ed46
Clubie <810443053@qq.com>
2025-02-22 20:12:43 +0800

    [WIP]

--- scripts/visualize_accelerate_effectiveness.py
+++ scripts/visualize_accelerate_effectiveness.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from matplotlib import patches as patches
 
 file_path = "results/accelerate_effectiveness.json"
-figure_path = "figures/accelerate_effectiveness.pdf"
+figure_path = "figures/accelerate_effectiveness.svg"
 model_path = "traces/opt-350m-16.json"
 model_name = "OPT-350M"
 min_bandwidth = 5e8
@@ -16,14 +16,14 @@ series = data[model_path]
 bandwidth = [x[0]/1e9 for x in series if min_bandwidth <= x[0] <= max_bandwidth]
 jct = [x[1] for x in series if min_bandwidth <= x[0] <= max_bandwidth]
 
-plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["font.family"] = ["Times New Roman", "SimSong"]
+plt.rcParams["font.size"] = 12
 plt.rcParams["pdf.fonttype"] = 42
 plt.rcParams["ps.fonttype"] = 42
-fig, ax = plt.subplots(figsize=(4,2))
-ax.grid(zorder=0)
+fig, ax = plt.subplots(figsize=(6,3))
 ax.plot(bandwidth, jct, label=model_name, color=(40/255,120/255,181/255), zorder=2)
-ax.set_xlabel("Aggregation bandwidth (GB/s)")
-ax.set_ylabel("Iteration duration (s)")
+ax.set_xlabel("通信带宽 (GB/s)")
+ax.set_ylabel("训练批次时长 (s)")
 ax.legend()
 
 plt.tight_layout(pad=0.5)
